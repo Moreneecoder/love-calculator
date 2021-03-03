60d96522696b92af4dd95c397fe9e9d5914cf574
Morenikeji Bello <caeserselassie@gmail.com>
2021-03-03 14:27:24 +0100

    Remove user class and user_spec

--- bin/main.rb
+++ bin/main.rb
@@ -3,3 +3,26 @@
 puts
 puts '################  LOVE CALCULATOR     #####################'
 puts
+
+print 'Enter Your Name: '
+username = gets.chomp.strip
+puts
+
+while username.empty?
+  print 'You must enter your name! Enter Your Name: '
+  username = gets.chomp.strip
+  puts
+end
+
+print 'Enter Your Partner\'s name: '
+partner_name = gets.chomp.strip
+puts
+
+while partner_name.empty?
+  print 'You must enter your partner\'s name! Enter Your Partner\'s Name: '
+  partner_name = gets.chomp.strip
+  puts
+end
+
+#   ASSIGN A RANDOM SCORE FOR PARTNERS
+
